8b56602f7e4e4a36fc41cd2c52fa6d0d931e6f43
Jack Honour <JaxkDev@gmail.com>
2021-08-20 13:45:01 +0100

    Small changes to dependency check.

--- src/JaxkDev/ChatBridge/Main.php
+++ src/JaxkDev/ChatBridge/Main.php
@@ -12,27 +12,41 @@
 
 namespace JaxkDev\ChatBridge;
 
+use JaxkDev\DiscordBot\Models\Messages\Message;
+use JaxkDev\DiscordBot\Plugin\ApiRejection;
+use JaxkDev\DiscordBot\Plugin\ApiResolution;
+use JaxkDev\DiscordBot\Plugin\Main as DiscordBot;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\VersionString;
 
 class Main extends PluginBase{
+
+    private DiscordBot $discord;
 
     public function onEnable(){
         if(!$this->checkDependencies()) return;
     }
 
     private function checkDependencies(): bool{
-        if(($discordBot = $this->getServer()->getPluginManager()->getPlugin("DiscordBot")) !== null){
-            if($discordBot->getDescription()->getWebsite() !== "https://github.com/DiscordBot-PMMP/DiscordBot"){
-                $this->getLogger()->critical("Incompatible dependency 'DiscordBot' detected, see https://github.com/DiscordBot-PMMP/DiscordBot/releases for the plugin.");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return false;
-            }
-            if((explode(".", ($ver = $discordBot->getDescription()->getVersion()))[0]??"") !== "2"){
-                $this->getLogger()->critical("Incompatible dependency 'DiscordBot' detected, v2.x.y is required, v$ver is available, see releases here https://github.com/DiscordBot-PMMP/DiscordBot/releases for compatible release version.");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return false;
-            }
+        $discordBot = $this->getServer()->getPluginManager()->getPlugin("DiscordBot");
+        if($discordBot === null) return false; //Will never happen.
+        if($discordBot->getDescription()->getWebsite() !== "https://github.com/DiscordBot-PMMP/DiscordBot"){
+            $this->getLogger()->critical("Incompatible dependency 'DiscordBot' detected, see https://github.com/DiscordBot-PMMP/DiscordBot/releases for the correct plugin.");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return false;
         }
+        $ver = new VersionString($discordBot->getDescription()->getVersion());
+        if($ver->getMajor() !== 2){
+            $this->getLogger()->critical("Incompatible dependency 'DiscordBot' detected, v2.x.y is required however v{$ver->getBaseVersion()}) is installed, see here https://github.com/DiscordBot-PMMP/DiscordBot/releases for downloads.");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return false;
+        }
+        if(!$discordBot instanceof DiscordBot){
+            $this->getLogger()->critical("Incompatible dependency 'DiscordBot' detected.");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return false;
+        }
+        $this->discord = $discordBot;
         return true;
     }
 }
