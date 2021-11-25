d1774f86f7098e7ccd0a43111dcf231966512732
Jack Honour <JaxkDev@gmail.com>
2021-11-25 17:20:17 +0000

    Disable OPCache on phpstan analysis.

---
+++ test/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * ChatBridge, PocketMine-MP Plugin.
+ *
+ * Licensed under the Open Software License version 3.0 (OSL-3.0)
+ * Copyright (C) 2020-2021 JaxkDev
+ *
+ * Twitter :: @JaxkDev
+ * Discord :: JaxkDev#2698
+ * Email   :: JaxkDev@gmail.com
+ */
+
+//JIT Should also be disabled for PHPStan analysis as it hangs on analysis.
+
+//OPCache should also be disabled because of https://github.com/phpstan/phpstan/issues/5503
+ini_set('opcache.enable', 'off');
